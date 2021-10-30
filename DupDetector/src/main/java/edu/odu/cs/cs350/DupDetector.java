bff8e6275e7df8fd2f05417e2264fc7e82f14bf5
cs_ahord001 <ahord001@odu.edu>
2021-10-30 13:53:17 -0400

    Driver class DupDetector.java

---
+++ DupDetector/src/main/java/edu/odu/cs/cs350/DupDetector.java
@@ -0,0 +1,16 @@
+package edu.odu.cs.cs350;
+
+public class DupDetector {
+
+	public static void main(int nSuggestions, String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		SourceCodeFiles currentSourceCodeFiles = new SourceCodeFiles();
+		System.out.println(args);
+		currentSourceCodeFiles.addFilePaths(args);
+		currentSourceCodeFiles.printFilePaths();
+
+	}
+
+}
