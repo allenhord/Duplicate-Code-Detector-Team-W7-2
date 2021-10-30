369dfb27514244ff967ff8735c4b455a936c8307
Kendra Schall <ksscha005@odu.edu>
2021-10-29 20:46:11 -0400

    attempt1

---
+++ src/main/DupDetector.java
@@ -0,0 +1,16 @@
+package edu.odu.cs.cs350.main;
+
+public class DupDetector {
+
+
+public final int MinSequenceLength = 10;
+public final int MaxSubstitutions = 8;
+
+public void optionalProperties(){
+    
+
+}
+
+
+
+}
