0d0d31f4afab1cdb981a1bb47a6347caddae17d0
Alexander Razikov <arazi002@odu.edu>
2021-10-31 01:58:51 -0400

    Set up projects properly to work with gradle wrapper
    Added the baseline DupDetector Class for Kendra
    Added the test class for the DupDetector
    Configured the settings in the build.gradle to now produce a proper .jar
    file
    configured the .gitgniore files

---
+++ lib/src/main/java/edu/odu/cs/cs350/DupDetector.java
@@ -0,0 +1,11 @@
+package edu.odu.cs.cs350;
+
+public class DupDetector {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
---
+++ lib/src/test/java/edu/odu/cs/cs350/DupDetectorTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.odu.cs.cs350;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Alex
+ *
+ */
+public class DupDetectorTest {
+
+	@Test
+	public void test() {
+		System.out.println("Not yet implemented");;
+	}
+
+}
