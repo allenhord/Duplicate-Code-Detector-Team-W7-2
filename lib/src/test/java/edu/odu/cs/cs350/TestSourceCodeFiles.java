6985ae774314a2b40e5fe3385adb67a40daa34fe
cs_ahord001 <ahord001@odu.edu>
2021-10-31 17:39:16 -0400

    Removed some junk functionsadded SourceCodeFile tests, refactored nSuggestions to use

--- lib/src/test/java/edu/odu/cs/cs350/TestSourceCodeFiles.java
+++ lib/src/test/java/edu/odu/cs/cs350/TestSourceCodeFiles.java
@@ -1,31 +1,35 @@
 package edu.odu.cs.cs350;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.Matchers.*;
+
 public class TestSourceCodeFiles{
 	
 	@Test
 	public void testAddFilePaths() {
-		//SourceCodeFiles m = new SourceCodeFiles();
-		//assertThat(m.fileSetToString(), not(contains("/file.cpp")));
-		//assertThat(m.printFilePaths(), not(containsString("/file.cpp")));
-		/*
-		m.addFilePaths("/file1.cpp");
-		m.printFilePaths();
-		m.addFilePaths("something/file2.cpp");
-		m.printFilePaths();
-		System.out.println(m.fileSetToString());
-		//assertThat(m.toString(), contains("/file1.cpp"));
-		//assertThat(m.printFilePaths(), containsString("/file1.cpp"));
-		//System.out.println(m.getFilePathArray());
-		//m.printFilePathsArray();
-		//System.out.println(Arrays.toString(filePathArray));
-		assertTrue(m.getNumCFiles() == 2);
-		System.out.println(m.getNumCFiles());
-		*/
+		SourceCodeFiles m = new SourceCodeFiles();
+		assertThat(m.getNumCFiles(), is(0));
+
+		m.addFilePathArray("/roll.cpp");
 		
+		assertThat(m.getFilePathArray(), hasItemInArray("/roll.cpp"));		
+		assertThat(m.getNumCFiles(), is(1));
 		
+		m.addFilePathArray("/roll.cpp");
+		assertThat(m.getNumCFiles(), is(1));	
+		
+		m.addFilePathArray("/fizzlebeef.h");
+		assertThat(m.getFilePathArray(), hasItemInArray("/fizzlebeef.h"));		
+		assertThat(m.getNumCFiles(), is(2));
 		
 	}
 	
+	@Test
+	public void testNumSuggestions() {
+		SourceCodeFiles m = new SourceCodeFiles();
+		m.setNumSuggestions(10);
+		assertThat(m.getNumSuggestions(), is (10));
 	
+	}
 }
